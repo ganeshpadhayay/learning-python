64c500ec5e04fa9311739a9fd80fde43199df49a
Ganesh Raj Padhayay <ganeshraj020794@gmail.com>
2020-06-10 20:19:27 +0530

    data passing from views to template

--- sample-blog-website/src/personal/views.py
+++ sample-blog-website/src/personal/views.py
@@ -3,5 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_screen_view(request):
-	print(request.headers)
-	return render(request, "personal/home.html", {})
+	context = {}
+	context['some_string'] = "this is some string I am passing to view"
+	context['some_number'] = 12345
+
+	# other way to use ocntext to pass the data to template from views class
+	#context = {
+	#		"some_string" : "this is some string I am passing to view",
+	#		"some_number" : 12345,
+	#}
+
+	list_of_values = []
+	list_of_values.append("first entry")
+	list_of_values.append("second entry")
+	list_of_values.append("third entry")
+	list_of_values.append("fourth entry")
+
+	context['list_of_values'] = list_of_values
+
+
+	return render(request, "personal/home.html", context)
